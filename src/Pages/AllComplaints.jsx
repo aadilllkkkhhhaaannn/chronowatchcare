d9404e663f5803af15f6df79a9cf689f55679caf
AadilKhan0461 <aadilkhan04610461@gmail.com>
2025-02-25 16:56:29 +0530

    frontend

--- src/Pages/AllComplaints.jsx
+++ src/Pages/AllComplaints.jsx
@@ -7,6 +7,7 @@ import Loading from "../Components/Loading";
 import { toast } from "react-toastify";
 
 const AllComplaints = () => {
+  const [samll, setSmall] = useState();
   const [isSmallScreen, setIsSmallScreen] = useState(window.innerWidth <= 320);
   const { watchName, isLoading, isError, isSuccess, message } = useSelector(
     (state) => state.watch
